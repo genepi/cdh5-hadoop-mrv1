be3a3dfb12d57ae25aee37680b9f3fc49f01a62f
Sebastian Schoenherr <seb.schoenherr@gmail.com>
2017-08-21 09:22:47 +0200

    Update adapt-mapred-config.sh

--- conf/adapt-mapred-config.sh
+++ conf/adapt-mapred-config.sh
@@ -4,3 +4,4 @@ line=$(($(grep -c ^processor /proc/cpuinfo)-1))
 hostname=$(cat /etc/hostname)
 sed s/AMOUNT/$line/ /usr/bin/mapred-site-template.xml > /etc/hadoop/conf.pseudo.mr1/mapred-site.xml
 sed s/HOSTNAME/$hostname/ /usr/bin/mapred-site-template.xml > /etc/hadoop/conf.pseudo.mr1/mapred-site.xml
+sed s/HOSTNAME/$hostname/ /usr/bin/core-site-template.xml > /etc/hadoop/conf.pseudo.mr1/core-site.xml
