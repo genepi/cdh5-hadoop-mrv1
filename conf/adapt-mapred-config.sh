3a2cd571078b8ce7b8995df08bd71cf5374f3a85
Sebastian Schoenherr <seb.schoenherr@gmail.com>
2017-08-21 09:19:19 +0200

    Update adapt-mapred-config.sh

--- conf/adapt-mapred-config.sh
+++ conf/adapt-mapred-config.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # calculates the amount of processors and sets the map and reduce tasks accordingly in mapred-site.xml
 line=$(($(grep -c ^processor /proc/cpuinfo)-1))
+hostname=$(cat /etc/hostname)
 sed s/AMOUNT/$line/ /usr/bin/mapred-site-template.xml > /etc/hadoop/conf.pseudo.mr1/mapred-site.xml
+sed s/HOSTNAME/$hostname/ /usr/bin/mapred-site-template.xml > /etc/hadoop/conf.pseudo.mr1/mapred-site.xml
