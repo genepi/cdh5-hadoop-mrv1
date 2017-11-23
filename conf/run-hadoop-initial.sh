d7b4fee60bdb1fc8927f4b887d0e369397e30c4e
Lukas Forer <lukas.forer@i-med.ac.at>
2017-11-23 18:14:49 +0100

    Update run-hadoop-initial.sh

--- conf/run-hadoop-initial.sh
+++ conf/run-hadoop-initial.sh
@@ -11,5 +11,10 @@ sudo /usr/bin/init-hdfs.sh
 # Start MapReduce Service
 for x in `cd /etc/init.d ; ls hadoop-0.20-mapreduce-*` ; do sudo service $x start ; done
 
+#print ip
+
+hostname=$(grep "$HOSTNAME" /etc/hosts|awk '{print $1}')
+echo "Congratulations! Cluster is running on $hostname"
+
 # end with command line
 /bin/bash
